80f8769cfb0d19e0c0c9a53b5fd117003dd7afaf
Bn4 <bnfour@bnfour.net>
2022-10-31 12:15:45 +0700

    actually write selected indices for audio and subs (0 if none selected) to file

--- per-folder-tracks.lua
+++ per-folder-tracks.lua
@@ -3,25 +3,54 @@
     overwriting previous config, if any. ]]
 
 -- configuration
-file_name = ".mpv"
 hotkey = "y"
+file_name = ".mpv"
+
+-- settings definition
+-- if changing, please note that spaces in these will break the very "sophisticated" parsing logic
+audio_key = "AUDIO"
+sub_key = "SUB"
 
 function on_load(event)
     local f = io.open(file_name, "r")
     if f ~= nil then
-        mp.osd_message("found, applying?")
         -- TODO read, apply
         f:close()
     end
     -- do nothing if file not found
 end
 
+-- store current audio and sub track indices to a file in the same folder
 function on_keypress(event)
     local f = io.open(file_name, "w")
-    -- TODO actual audio/sub track ids/indices/whatever
-    f:write("test message please ignore")
+    f:write(string.format("%s %d\n", audio_key, get_audio_id()))
+    f:write(string.format("%s %d\n", sub_key, get_sub_id()))
     f:close()
-    mp.osd_message("(not yet) Stored current audio/sub tracks selection")
+    mp.osd_message("Stored audio+sub track selection")
+end
+
+-- get_*_id return 0 instead of nil for disabled tracks
+-- in order to keep the config file simplier by storing only ints as values
+-- (is it really simplier though?)
+
+function get_audio_id()
+    local cur_audio_id = mp.get_property_number("current-tracks/audio/id")
+    if cur_audio_id ~= nil
+    then
+        return cur_audio_id
+    else
+        return 0
+    end
+end
+
+function get_sub_id()
+    local cur_sub_id = mp.get_property_number("current-tracks/sub/id")
+    if cur_sub_id ~= nil
+    then
+        return cur_sub_id
+    else
+        return 0
+    end
 end
 
 mp.register_event("file-loaded", on_load)
