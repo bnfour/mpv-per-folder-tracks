7b06aed228ea5e8d6bda126a6964702dccf9bc9a
Bn4 <bnfour@bnfour.net>
2022-10-31 11:32:21 +0700

    file io, some configurability

--- per-folder-tracks.lua
+++ per-folder-tracks.lua
@@ -2,14 +2,27 @@
     On hotkey, write current settings to the file,
     overwriting previous config, if any. ]]
 
--- just making sure this works
+-- configuration
+file_name = ".mpv"
+hotkey = "y"
+
 function on_load(event)
-    mp.osd_message("file-loaded fired")
+    local f = io.open(file_name, "r")
+    if f ~= nil then
+        mp.osd_message("found, applying?")
+        -- TODO read, apply
+        f:close()
+    end
+    -- do nothing if file not found
 end
 
 function on_keypress(event)
-    mp.osd_message("keypress!")
+    local f = io.open(file_name, "w")
+    -- TODO actual audio/sub track ids/indices/whatever
+    f:write("test message please ignore")
+    f:close()
+    mp.osd_message("(not yet) Stored current audio/sub tracks selection")
 end
 
 mp.register_event("file-loaded", on_load)
-mp.add_key_binding("y", on_keypress)
+mp.add_key_binding(hotkey, on_keypress)
